4908bbcf28f96398bb2f2268cc76f2dcc8c4b929
OsmarParra <pipepamu@gmail.com>
2021-08-26 16:20:58 -0500

    Firmado por: OsmarParra <pipepamu@gmail.com>

--- Main.java
+++ Main.java
@@ -1,16 +1,36 @@
-public class Main
+package sesion05_cg_mul_b;
+import java.util.Scanner;
 {
 	public static void main(String[] args) {
 		// Defina y asigne dos objetos, un Cadrado y una Circunferencia
 		// Solicite al usuario ingresar el valor de un vector de traslación 
 		// Aplique el vector de traslación anterior a cada uno de los objetos referidos
 		// Muestre las posiciones originales y finales de cada uno de los objetos referidos
-		
+		 int x,y;
+		 System.out.println ("Introduzca valores del vector de Traslacion: ");
+		 System.out.println ("x: ");
+		 Scanner entrada = new Scanner(System.in);
+		 x = entrada.nextInt();
+		 System.out.println ("y: ");
+		 y = entrada.nextInt();
 	}
 }
 
 class Coord {
     private int x, y;
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+     public void setX(int x){
+        this.x = x;
+    }
+       public void setY(int y){
+        this.y = y;
+    }
+    
 }
 
 class Cuadrado {
@@ -20,11 +40,12 @@ class Cuadrado {
         this.c = c;
         this.lado = lado;
     }
-    public void Traslacion() {  // Complete los parámetros requeridos
-        
+    public void Traslacion(Coord tr) {  // Complete los parámetros requeridos
+        c.setX(tr.getX()+c.getX());
+        c.setY(tr.getY()+c.getY());
     }
-    public void Escalado() {  // Complete los parámetros requeridos
-        
+    public void Escalado(int es) {  // Complete los parámetros requeridos
+        lado = (int)lado*(1+es/100.0f)
     }
     public void Rotacion() {  // Complete los parámetros requeridos
         
@@ -36,5 +57,3 @@ class Circunferencia {
     float radio;
     // Implementar los métodos Traslacion, Escalado y Rotacion para ésta clase
 }
-
-
